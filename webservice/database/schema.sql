d9f87753340c7ad314818797f74951fb80968383
Jason Runzer <jason.runzer@uoit.net>
2016-10-22 19:02:44 -0700

    added user to tiles, and stats to users schema

--- webservice/database/schema.sql
+++ webservice/database/schema.sql
@@ -3,11 +3,17 @@ CREATE TABLE Users (
     password varchar(255) NOT NULL,
     gold int DEFAULT 0,
     food int DEFAULT 0,
-    tiles int DEFAULT 0
+    tiles int DEFAULT 0,
+    tilesTaken int DEFAULT 0,
+    goldObtained int DEFAULT 0,
+    foodObtained int DEFAULT 0
 );
 
 CREATE TABLE Tiles (
     tileID int NOT NULL PRIMARY KEY,
     latitude float(24),
-    longitude float(24)
+    longitude float(24),
+    username varchar(255),
+    FOREIGN KEY(username) REFERENCES Users(username)
 );
+
