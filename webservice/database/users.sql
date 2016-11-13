f83214958aaad71e6659f5ba7e4f3125675f4477
Jason Runzer <jason.runzer@uoit.net>
2016-11-12 18:49:53 -0800

    added collect resources route

--- webservice/database/users.sql
+++ webservice/database/users.sql
@@ -1,7 +1,7 @@
 INSERT INTO users (Username, password)
 VALUES (
     'jason',
-    '$2a$10$8Xq9EsZ9tuVgvEzVXQB3n.Tu.CXmY5RgnjN87SSQWRoeYJnZaj6lK'
+    '$2a$10$kWYi0dsG0SPw4NONHoUMJeXcR9NJWWHbBHYwcM5K6ec/xN5w2a72O'
 );
 
 INSERT INTO Tiles ("tileLatID", "tileLngID", "username", "gold", "food")
--- webservice/routes/users.js
+++ webservice/routes/users.js
@@ -59,3 +59,25 @@ router.post('/subtract', auth.authenticate(), function(req, res, next) {
   return res.status(200).json({"success": "subtract"});
 });
 module.exports = router;
+
+/* POST Collect Resources */
+router.post('/collect', auth.authenticate(), function(req, res, next) {
+  db.any('SELECT "goldObtained", "foodObtained" ' +
+        'FROM Users ' + 
+        'WHERE "username" = $1;',
+        [req.body.username])
+  .then(function(data) {
+    db.none('UPDATE Users ' +
+          'SET "goldObtained" = "goldObtained" - $2, ' +
+          '"foodObtained" = "foodObtained" - $3 ' +
+          'WHERE "username" = $1;',
+          [req.body.username, data[0].goldObtained, data[0].foodObtained])
+    .catch(function(error) {
+      console.log(error);
+    });
+    res.status(200).json({goldObtained: data[0].goldObtained, foodObtained: data[0].foodObtained});
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
+});
